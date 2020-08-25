bc096380528e4e0e55bd9dcf10f7499ec1d3b183
kento <kento@okura.at>
2020-08-25 11:02:36 +0200

    ADD A LOT OF FILES
    scratchpads dont work

--- config.def.h
+++ config.def.h
@@ -27,10 +27,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "Thunderbird", "Msgcompose" ,NULL,     0,         1,          0,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1,	0 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1,	0 },
+	{ "Thunderbird", "Msgcompose" ,NULL,   0,         1,          0,           0,        -1,	0 },
+	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1,       's' }, /* xev */
 };
 
 /* layout(s) */
@@ -55,15 +55,16 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|Mod1Mask,              KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY|Mod1Mask|ShiftMask,    KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *scratchpadcmd[] = {"s", "st", "-t", "scratchpad", NULL}; 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -81,9 +82,9 @@ static Key keys[] = {
 
 //	{ MODKEY,             	        XK_u,      setcfact,       {.f = +0.25} },
 //	{ MODKEY,         	        XK_i,      setcfact,       {.f = -0.25} },
-	{ MODKEY,	                XK_o,      setcfact,       {.f =  0.00} },
+//	{ MODKEY,	                XK_o,      setcfact,       {.f =  0.00} },
 //	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-//	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
 //	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 //	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 //	{ MODKEY,                       XK_space,  setlayout,      {.v = &layouts[1]} },
@@ -92,23 +93,24 @@ static Key keys[] = {
 //	{ MODKEY,                       XK_x,      setlayout,      {.v = &layouts[4]} },
 //	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[5]} },
 //	{ MODKEY,                       XK_v,      setlayout,      {.v = &layouts[6]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+//	{ MODKEY,                       XK_space,  setlayout,      {0} },
 //	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 //	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+//	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 //	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 //	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 //	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 //	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-//	TAGKEYS(                        XK_1,                      0)
-//	TAGKEYS(                        XK_2,                      1)
-//	TAGKEYS(                        XK_3,                      2)
-//	TAGKEYS(                        XK_4,                      3)
-//	TAGKEYS(                        XK_5,                      4)
-//	TAGKEYS(                        XK_6,                      5)
-//	TAGKEYS(                        XK_7,                      6)
-//	TAGKEYS(                        XK_8,                      7)
-//	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
 //	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
@@ -121,7 +123,7 @@ static Button buttons[] = {
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY|ShiftMask, Button3,      resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask, Button1,      resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
